6829516f234ac88104d6ddd0cd131b5fd790c34d
clarjohn <59753565+clarjohn@users.noreply.github.com>
2020-03-12 15:28:23 -0700

    start week 8 project

---
+++ DBIntro.js
@@ -0,0 +1,52 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 7034);
+
+app.get('/',function(req,res){
+  res.render('home');
+});
+
+
+
+/*
+app.get('/reset-table',function(req,res,next){
+    var context = {};
+    [your connection pool].query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+      var createString = "CREATE TABLE workouts("+
+      "id INT PRIMARY KEY AUTO_INCREMENT,"+
+      "name VARCHAR(255) NOT NULL,"+
+      "reps INT,"+
+      "weight INT,"+
+      "date DATE,"+
+      "lbs BOOLEAN)";
+      [your connection pool].query(createString, function(err){
+        context.results = "Table reset";
+        res.render('home',context);
+      })
+    });
+  });
+*/
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});

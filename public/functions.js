e130944c4b3efb526a949917ce8435e0d745dca8
clarjohn <59753565+clarjohn@users.noreply.github.com>
2020-03-13 17:12:25 -0700

    x

--- public/functions.js
+++ public/functions.js
@@ -2,7 +2,7 @@
 //document.addEventListener('DOMContentLoaded', loadTable);
 
 
-
+/*
 function loadTable(){
     document.getElementById("test").textContent = "This Test Worked still";
     var request = new XMLHttpRequest();
@@ -34,6 +34,8 @@ function showtable(tabDat) {
       }
     }
   }
+*/
+
 
 function addExc(){
     document.getElementById('Load_ID').addEventListener('click', function(event){
@@ -43,7 +45,7 @@ function addExc(){
         payload.reps = document.getElementById('reps_input').value;
         payload.weight = document.getElementById('weight_input').value;
         payload.date = document.getElementById('date_input').value;
-
+        console.log("Send Request");
         request2.open('post', 'http://flip3.engr.oregonstate.edu:7808/', true);
         request2.setRequestHeader('Content-Type', 'application/json');
         request2.addEventListener('load',function(){
